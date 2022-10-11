fe2d025da71fdc4c68dda15d6cd0f4f71cdd1a31
GioKatamadze <105790095+GioKatamadze@users.noreply.github.com>
2022-10-11 21:34:22 +0400

    Update: myScript.js

--- myScript.js
+++ myScript.js
@@ -1,33 +1,35 @@
+const tipButtons = Array.from(document.querySelectorAll(".tipbtn"));
+const tip5 = document.getElementById("tip5");
 let bill = document.getElementById("billAmount");
 let people = document.getElementById("peopleAmount");
 let reset = document.getElementById("reset");
-const tipButtons = Array.from(document.querySelectorAll(".tipbtn"));
-const tip5 = document.getElementById("tip5");
 let customTip = document.getElementById("customAmount");
 let tipPerPerson = document.querySelector(".tipPerPersonDigits");
 let totalPerPerson = document.querySelector(".totalPerPersonDigits");
 let tipNumber;
 
 
-
-
-
-
+// Calculation Functions
 
 function calcFromBill () {
-    tipPerPerson.textContent = parseFloat((bill.value * `0.05`) / 1 ).toFixed(2); 
-    totalPerPerson.textContent = parseFloat((bill.value / 1) + (bill.value * `0.05`) / 1 ).toFixed(2);  
+    tipPerPerson.textContent = parseFloat((bill.value * 0.05) / 1 ).toFixed(2); 
+    totalPerPerson.textContent = parseFloat((bill.value / 1) + (bill.value * 0.05) / 1).toFixed(2);  
 }
 
 function calcFromPeople () {
-    tipPerPerson.textContent = parseFloat((bill.value * `0.05`) / people.value ).toFixed(2); 
-    totalPerPerson.textContent = parseFloat((bill.value / people.value) + (bill.value * `0.05`) / people.value).toFixed(2);  
+    tipPerPerson.textContent = parseFloat((bill.value * 0.05) / people.value ).toFixed(2); 
+    totalPerPerson.textContent = parseFloat((bill.value / people.value) + (bill.value * 0.05) / people.value).toFixed(2);  
 }
 
 function calcFromTip () {
     tipPerPerson.textContent = parseFloat((bill.value * tipNumber) / people.value ).toFixed(2); 
     totalPerPerson.textContent = parseFloat((bill.value / people.value) + (bill.value * tipNumber) / people.value).toFixed(2); 
 }
+
+
+
+
+// Number and Button Configurer
 
 function noNumber () {
     if (isNaN (tipPerPerson.textContent)) {
@@ -38,28 +40,88 @@ function noNumber () {
     }
 };
 
-
-
-
-
-
-
-
-
-
-
 let disableButton = () => {
     reset.style.backgroundColor = "#0D686D";
     reset.style.color = "rgba(0, 71, 75, 0.35)";
 };
 
-let enableButton = () => {
+function enableButton () {
+    if (bill.value > 0) {
+        reset.style.backgroundColor = "#9FE8DF";
+        reset.style.color = "#00474B";
+    }  
+    if (people.value > 0) {
+        reset.style.backgroundColor = "#9FE8DF";
+        reset.style.color = "#00474B";
+    } 
+};
+
+
+
+
+
+
+// Valiation Functions
+
+function billValidation () {
+    if (bill.value < 0 ) {
+        bill.value = "";
+        bill.style.border = "4px solid red"
+        tipPerPerson.textContent = "0.00";
+        totalPerPerson.textContent = "0.00";
+        reset.style.backgroundColor = "#9FE8DF";
+        reset.style.color = "#00474B";
+        document.getElementById("zero1").style.display = "block";
+        document.getElementById("zero1").style.marginBottom = "-20px"
+    }
+    if (bill.value > 1000000) {
+        bill.value = 1000000;
+        bill.style.border = "4px solid red"
+        tipPerPerson.textContent = "0.00";
+        totalPerPerson.textContent = "0.00";
+        enableButton ();
+        document.getElementById("high1").style.display = "block";
+        document.getElementById("high1").style.marginBottom = "-20px"
+    }
+}
+
+function peopleValidation () {
+    if (people.value < 0) {
+        people.value = "";
+        people.style.border = "4px solid red"
+        tipPerPerson.textContent = "0.00";
+        totalPerPerson.textContent = "0.00";
+        reset.style.backgroundColor = "#9FE8DF";
+        reset.style.color = "#00474B";
+        document.getElementById("zero2").style.display = "block";
+        document.getElementById("zero2").style.marginBottom = "-20px"
+    }
+    if (people.value > 1000000) {
+        people.value = 1000000;
+        people.style.border = "4px solid red"
+        tipPerPerson.textContent = "0.00";
+        totalPerPerson.textContent = "0.00";
+        enableButton ();
+        document.getElementById("high2").style.display = "block";
+        document.getElementById("high2").style.marginBottom = "-20px"
+    }
+}
+
+function customTipValidation () {
+    if (customTip.value > 99999) {
+        customTip.value = 99999;
+        customTip.style.border = "4px solid red"
+        tipPerPerson.textContent = "0.00";
+        totalPerPerson.textContent = "0.00";
+    }
+    if (customTip.value <= 0) {
+        customTip.value = "";
+        customTip.style.border = "4px solid red"
+        tipPerPerson.textContent = "0.00";
+        totalPerPerson.textContent = "0.00";
+    }
     reset.style.backgroundColor = "#9FE8DF";
     reset.style.color = "#00474B";
-};
-let activetip5 = () => {
-    tip5.style.backgroundColor = "#9FE8DF";
-    tip5.style.color = "#00474B"
 }
 
 
@@ -67,62 +129,81 @@ let activetip5 = () => {
 
 
 
-
-
-
+// Calculator Process
 
 disableButton();
 
-bill.addEventListener("input", function() {
-    activetip5();
-    people.value = "1";
-    calcFromBill ()
-    enableButton();
-    noNumber ()
-    
-
-    people.addEventListener("input", function () {
-        activetip5();
-        calcFromPeople () 
-        enableButton();
-        noNumber ()
+for (let i = 0; i < tipButtons.length ; i ++) {
+    tipButtons[i].addEventListener("click", function (event) {
+        if (bill.value > 0 && bill.value < 9999999 && people.value > 0 && people.value < 999999) {
+            tipNumber = parseFloat(event.target.textContent) / 100;
+            calcFromTip ();
+            enableButton(); 
+            noNumber () ;
+            bill.style.border = "";
+            people.style.border = "";
+        }
     });
+    billValidation ();
+    peopleValidation();
+};
+
+customTip.addEventListener("input", function () {
+    if (bill.value > 0 && bill.value < 9999999 && people.value > 0 && people.value < 999999) {
+        tipNumber = customTip.value / 100; 
+        enableButton();
+        calcFromTip ();
+        noNumber ();
+        bill.style.border = "";
+        people.style.border = "";
+    };
+    billValidation ();
+    peopleValidation();
+    customTipValidation ();
+});
+
+bill.addEventListener("input", function() {
+    if (bill.value > 0 && bill.value < 9999999 && people.value > 0 && people.value < 999999) {
+        calcFromBill ();
+        noNumber ();
+        reset.style.backgroundColor = "#9FE8DF";
+        reset.style.color = "#00474B";
+        bill.style.border = "";
+        people.style.border = "";
+    };
+    billValidation ();
+});
+
+people.addEventListener("input", function () {
+    if (bill.value > 0 && bill.value < 9999999 && people.value > 0 && people.value < 999999) {
+        calcFromPeople ();
+        noNumber ();
+        reset.style.backgroundColor = "#9FE8DF";
+        reset.style.color = "#00474B";
+        bill.style.border = "";
+        people.style.border = "";
+    };
+    peopleValidation();
 });
 
 
 
 
 
-for (let i = 0; i < tipButtons.length ; i ++) {
-    tipButtons[i].addEventListener("click", function (event) {
-        tipNumber = parseFloat(event.target.textContent) / 100;
-        calcFromTip ()
-        enableButton(); 
-        noNumber () 
-         
-        customTip.addEventListener("input", function () {
-            tipNumber = customTip.value / 100; 
-            calcFromTip ()
-            enableButton();
-            noNumber ()
-        });
-    });
-   
-};
-
-
-
-
-
-
-
-
+// Reset
 
 reset.onclick = function () {
+    disableButton();
     bill.value = "";
     people.value = "";
     customTip.value = "";
+    bill.style.border = "";
+    people.style.border = "";
+    customTip.style.border = ""
     tipPerPerson.textContent = "0.00";
     totalPerPerson.textContent = "0.00";
-    disableButton();
+    document.getElementById("high1").style.display = "none";
+    document.getElementById("high2").style.display = "none";
+    document.getElementById("zero1").style.display = "none";
+    document.getElementById("zero2").style.display = "none";
 }
