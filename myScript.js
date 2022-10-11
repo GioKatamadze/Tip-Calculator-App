9d2717e7fc7ae87a05fc06d4ada9c2bb826e3fcc
GioKatamadze <105790095+GioKatamadze@users.noreply.github.com>
2022-10-11 17:11:46 +0400

    update: myScript.js

--- myScript.js
+++ myScript.js
@@ -1,93 +1,120 @@
-const bill = document.getElementById("billAmount");
-const people = document.getElementById("peopleAmount");
-const reset = document.getElementById("reset");
+let bill = document.getElementById("billAmount");
+let people = document.getElementById("peopleAmount");
+let reset = document.getElementById("reset");
 const tipButtons = Array.from(document.querySelectorAll(".tipbtn"));
 const tip5 = document.getElementById("tip5");
-
 let customTip = document.getElementById("customAmount");
 let tipPerPerson = document.querySelector(".tipPerPersonDigits");
 let totalPerPerson = document.querySelector(".totalPerPersonDigits");
-
-let tipPerPersonResult;
-let totalPerPersonResult;
-let billValue = bill.value;
-let peopleValue = people.value;
 let tipNumber;
 
 
 
-bill.addEventListener("input", function () {  
-    billValue = parseFloat(bill.value); 
-    peopleValue = parseFloat(people.value);
-
-    tipPerPerson.textContent = parseFloat((billValue * tipNumber) / peopleValue ).toFixed(2); 
-    totalPerPerson.textContent = parseFloat((billValue / peopleValue) + (billValue * tipNumber) / peopleValue ).toFixed(2);  
 
 
+
+
+function calcFromBill () {
+    tipPerPerson.textContent = parseFloat((bill.value * `0.05`) / 1 ).toFixed(2); 
+    totalPerPerson.textContent = parseFloat((bill.value / 1) + (bill.value * `0.05`) / 1 ).toFixed(2);  
+}
+
+function calcFromPeople () {
+    tipPerPerson.textContent = parseFloat((bill.value * `0.05`) / people.value ).toFixed(2); 
+    totalPerPerson.textContent = parseFloat((bill.value / people.value) + (bill.value * `0.05`) / people.value).toFixed(2);  
+}
+
+function calcFromTip () {
+    tipPerPerson.textContent = parseFloat((bill.value * tipNumber) / people.value ).toFixed(2); 
+    totalPerPerson.textContent = parseFloat((bill.value / people.value) + (bill.value * tipNumber) / people.value).toFixed(2); 
+}
+
+function noNumber () {
     if (isNaN (tipPerPerson.textContent)) {
         tipPerPerson.textContent = "0.00";
     }
     if (isNaN (totalPerPerson.textContent)) {
         totalPerPerson.textContent = "0.00";
     }
+};
+
+
+
+
+
+
+
+
+
+
+
+let disableButton = () => {
+    reset.style.backgroundColor = "#0D686D";
+    reset.style.color = "rgba(0, 71, 75, 0.35)";
+};
+
+let enableButton = () => {
+    reset.style.backgroundColor = "#9FE8DF";
+    reset.style.color = "#00474B";
+};
+let activetip5 = () => {
+    tip5.style.backgroundColor = "#9FE8DF";
+    tip5.style.color = "#00474B"
+}
+
+
+
+
+
+
+
+
+
+
+disableButton();
+
+bill.addEventListener("input", function() {
+    activetip5();
+    people.value = "1";
+    calcFromBill ()
+    enableButton();
+    noNumber ()
+    
+
+    people.addEventListener("input", function () {
+        activetip5();
+        calcFromPeople () 
+        enableButton();
+        noNumber ()
+    });
 });
- 
 
 
-people.addEventListener("input", function () {
-    billValue = parseFloat(bill.value); 
-    peopleValue = parseFloat(people.value);
-    peopleValue = parseFloat(event.target.textContent)
-
-    tipPerPerson.textContent = parseFloat((billValue * tipNumber) / peopleValue ).toFixed(2); 
-    totalPerPerson.textContent = parseFloat((billValue / peopleValue) + (billValue * tipNumber) / peopleValue ).toFixed(2);    
-
-
-    if (isNaN (tipPerPerson.textContent)) {
-        tipPerPerson.textContent = "0.00";
-    }
-    if (isNaN (totalPerPerson.textContent)) {
-        totalPerPerson.textContent = "0.00";
-    }
-});
 
 
 
 for (let i = 0; i < tipButtons.length ; i ++) {
     tipButtons[i].addEventListener("click", function (event) {
-        billValue = parseFloat(bill.value); 
-        peopleValue = parseFloat(people.value);
         tipNumber = parseFloat(event.target.textContent) / 100;
-
-        tipPerPerson.textContent = parseFloat((billValue * tipNumber) / peopleValue ).toFixed(2); 
-        totalPerPerson.textContent = parseFloat((billValue / peopleValue) + (billValue * tipNumber) / peopleValue ).toFixed(2);    
+        calcFromTip ()
+        enableButton(); 
+        noNumber () 
          
-        
-        if (isNaN (tipPerPerson.textContent)) {
-            tipPerPerson.textContent = "0.00";
-        }
-        if (isNaN (totalPerPerson.textContent)) {
-            totalPerPerson.textContent = "0.00";
-        }
+        customTip.addEventListener("input", function () {
+            tipNumber = customTip.value / 100; 
+            calcFromTip ()
+            enableButton();
+            noNumber ()
+        });
     });
    
-}; 
+};
 
-customTip.addEventListener("input", function () {
-    billValue = parseFloat(bill.value); 
-    peopleValue = parseFloat(people.value);
-    tipNumber = customTip.value / 100; 
 
-    tipPerPerson.textContent = parseFloat((billValue * tipNumber) / peopleValue ).toFixed(2); 
-    totalPerPerson.textContent = parseFloat((billValue / peopleValue) + (billValue * tipNumber) / peopleValue ).toFixed(2); 
 
-    if (isNaN (tipPerPerson.textContent)) {
-        tipPerPerson.textContent = "0.00";
-    }
-    if (isNaN (totalPerPerson.textContent)) {
-        totalPerPerson.textContent = "0.00";
-    }
-});
+
+
+
 
 
 
@@ -97,5 +124,5 @@ reset.onclick = function () {
     customTip.value = "";
     tipPerPerson.textContent = "0.00";
     totalPerPerson.textContent = "0.00";
-   
-};
+    disableButton();
+}
